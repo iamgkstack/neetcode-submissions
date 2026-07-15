5d413b13afe4d625fb5305011d7afd11c33a8f1c
Gaurav Kumar <gauravkumar5295@gmail.com>
2026-07-15 07:34:27 +0530

    Add: longest-consecutive-sequence - submission-1

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-1.go
@@ -0,0 +1,33 @@
+func longestConsecutive(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+
+	set := make(map[int]bool)
+
+	for _, num := range nums {
+		set[num] = true
+	}
+
+	longest := 0
+
+	for num := range set {
+		if !set[num-1] {
+
+			current := num
+			length := 1
+
+			for set[current+1] {
+				current++
+				length++
+			}
+
+			if length > longest {
+				longest = length
+			}
+
+		}
+	}
+
+    return longest
+}
